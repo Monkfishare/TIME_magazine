cbb50c770e3bfd61f2cf1d74af5c475c8a3ffce3
Monkfishare <50627932+Monkfishare@users.noreply.github.com>
2024-03-17 00:56:12 +0800

    Update replace.py

--- replace.py
+++ replace.py
@@ -1,6 +1,6 @@
 recipe_path = "time_magazine.recipe"
 
-Edition_ID = "4748217"
+Edition_ID = "4594940"
 # For past edition, for example, Edition_ID = '6548003'
 # https://raw.githubusercontent.com/Monkfishare/TIME_magazine/main/TIME_magazine_database.txt
 
